f33ed370c67c3bfd120d5265f455808458e4b388
Hafiz Shaikh Abdul Sami <65606480+ShaikhAbdulSami@users.noreply.github.com>
2023-05-16 00:13:12 +0500

    DB Connection

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const env = require("dotenv");
+const env = require("dotenv").config();
+const mongoose = require("mongoose");
+
+// Routes
 const homeView = require("./routes/index")
+
 //Creating Server
 const app = express();
 //Setting Templating View Engine
@@ -10,17 +14,23 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 // Using Cookie Parser
 app.use(cookieParser());
-// Configuring dotenv
-env.config();
 
 //Defining Port
 const port = process.env.PORT || 3000;
+const uri = process.env.DB_URI;
 
-// Listening 
-app.listen(port, () => {
-    console.log(
-        `Server Has Been Started on ${port}`
-    );
-});
+//Connecting DB with Server
+mongoose
+    .connect(uri)
+    .then((result) => {
+        app.listen(port, () => {
+            console.log(
+                `Server Has Been Started on ${port} & DB is Connected Successfully`
+            );
+        });
+    })
+    .catch((err) => {
+        console.log(`DB Connection Error ${err}`);
+    });
 
 app.use('/', homeView)
